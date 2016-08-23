4db330d3b9c38aa60c9a6e7059763296df3248df
Whitney Belba <whitneybelba@gmail.com>
2016-08-23 00:42:34 +0000

    finally fixed tables to correctly display categories

--- server.py
+++ server.py
@@ -28,26 +28,54 @@ def get_choices():
 
     location = request.args.get("location") + request.args.get("state")
     radius = request.args.get("radius")
-
     restaurant_categories = request.args.getlist("restaurant")
+    bar_categories = request.args.getlist("bar")
+    activity_categories = request.args.getlist("activity")
+
+                ############################################
+                #        yelp query for restaurants        #
+                ############################################
     # iterates over list of categories selected from checkboxes, calls
     # the search_yelp function for each category, and appends the returned
     # list of restaurants in a category to a list for all restaurants
     restaurant_list = []
     for category in restaurant_categories:
         rest_results = search_yelp(category, location, radius)
+        for restaurant in rest_results:
+            categories = []
+            for category in restaurant['categories']:
+                categories.append(category[0])
+            restaurant['category'] = ", ".join(categories)
+
         restaurant_list.append(rest_results)
 
-    bar_categories = request.args.getlist("bar")
+                ############################################
+                #            yelp query for bars           #
+                ############################################
     bar_list = []
     for bar in bar_categories:
         bar_results = search_yelp(bar, location, radius)
+        for bar in bar_results:
+            categories = []
+            for category in bar['categories']:
+                categories.append(category[0])
+            bar['category'] = ", ".join(categories)
+
         bar_list.append(bar_results)
 
-    activity_categories = request.args.getlist("activity")
+                ############################################
+                #         yelp query for activities        #
+                ############################################
+
     activity_list = []
     for activity in activity_categories:
         activity_results = search_yelp(activity, location, radius)
+        for activity in activity_results:
+            categories = []
+            for category in activity['categories']:
+                categories.append(category[0])
+            activity['category'] = ", ".join(categories)
+
         activity_list.append(activity_results)
 
     return render_template("results.html",
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,9 @@
-$('submit').click(function() {
-    alert(/[a-z ]+, [a-z]{2} /.test($('input').val()));
+var city = document.getElementById("city");
+
+city.addEventListener("submit", function (event) {
+  if (city.validity.typeMismatch) {
+    email.setCustomValidity("Please enter a state");
+  } else {
+    email.setCustomValidity("");
+  }
 });
